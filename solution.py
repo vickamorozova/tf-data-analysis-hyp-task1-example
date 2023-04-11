35f7dc55a85c37615244d9be44a6831988cfbf2e
Viktoriya Morozova <117348526+vickamorozova@users.noreply.github.com>
2023-04-11 08:27:47 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -11,4 +11,21 @@ def solution(x_success: int,
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    
+    # Рассчитываем пропорции для каждой выборки
+    p1 = x_success / x_cnt
+    p2 = y_success / y_cnt
+    
+    # Рассчитываем объединенную пропорцию
+    p_combined = (x_success + y_success) / (x_cnt + y_cnt)
+    
+    # Рассчитываем статистику теста
+    z = (p1 - p2) / np.sqrt(p_combined * (1 - p_combined) * (1 / x_cnt + 1 / y_cnt))
+    
+    # Рассчитываем двухсторонний p-value
+    p_value = 2 * (1 - norm.cdf(abs(z)))
+    
+    # Сравниваем p-value с уровнем значимости и принимаем решение
+    alpha = 0.08
+    return p_value < alpha
+   
