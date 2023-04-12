966839f03744198c6ba1c06780eeb25eae1e171d
Viktoriya Morozova <117348526+vickamorozova@users.noreply.github.com>
2023-04-12 08:35:34 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-
+from statsmodels.stats.proportion import proportions_ztest
 
 chat_id = 357282961 # Ваш chat ID, не меняйте название переменной
 
@@ -12,20 +12,9 @@ def solution(x_success: int,
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
     
-    # Рассчитываем пропорции для каждой выборки
-    p1 = x_success / x_cnt
-    p2 = y_success / y_cnt
-    
-    # Рассчитываем объединенную пропорцию
-    p_combined = (x_success + y_success) / (x_cnt + y_cnt)
-    
-    # Рассчитываем статистику теста
-    z = (p1 - p2) / np.sqrt(p_combined * (1 - p_combined) * (1 / x_cnt + 1 / y_cnt))
-    
-    # Рассчитываем двухсторонний p-value
-    p_value = 2 * (1 - norm.cdf(abs(z)))
-    
-    # Сравниваем p-value с уровнем значимости и принимаем решение
+    _, pval = proportions_ztest([x_success, y_success], [x_cnt, y_cnt], alternative='larger')
     alpha = 0.08
-    return p_value < alpha
+    effect = (pval < alpha)
+    
+    return effect 
    
