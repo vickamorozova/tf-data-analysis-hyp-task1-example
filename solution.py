200f65d6002eb59556ae437c392e822814ede77d
Viktoriya Morozova <117348526+vickamorozova@users.noreply.github.com>
2023-04-13 07:09:12 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -8,10 +8,11 @@ def solution(x_success: int,
              x_cnt: int, 
              y_success: int, 
              y_cnt: int) -> bool:
+    # Измените код этой функции
+    # Это будет вашим решением
+    # Не меняйте название функции и её аргументы
+    _, pval = proportions_ztest([x_success, y_success], [x_cnt, y_cnt], alternative='larger')
+    alpha = 0.08
+    effect = (pval < alpha)
     
-    _, pvalue = proportions_ztest([x_success, y_success], [x_cnt, y_cnt], alternative='larger')
-
-    # выявляем наличие эффекта
-    effect = (pvalue < 0.08)
-    
-    return effect
+    return effect # Ваш ответ, True или False
